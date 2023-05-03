c0c44937045978244c511f71f01c382103e4a02a
Braedon-Trumpie-Student <112421997+Braedon-Trumpie-Student@users.noreply.github.com>
2023-05-03 12:11:14 -0400

    hefty push

---
+++ Assets/Scripts/ControlsManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsManager : MonoBehaviour
+{
+    [Header("Debug Controls - 0\nKeyboard And Controller - 1")]
+    public int controls;
+}
