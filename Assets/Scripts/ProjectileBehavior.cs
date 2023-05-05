8e03589fc1e95ba4a8867f6c44fee68072f081c1
Braedon-Trumpie-Student <112421997+Braedon-Trumpie-Student@users.noreply.github.com>
2023-05-05 14:42:52 -0400

    bouncing bullets

--- Assets/Scripts/ProjectileBehavior.cs
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -7,9 +7,36 @@ public class ProjectileBehavior : MonoBehaviour
     public float projectileSpeed = 30f;
     Rigidbody2D rb;
     [HideInInspector] public PlayerController projectileOwner;
-    void OnTriggerEnter2D(Collider2D other) 
+    Vector2 currentVelocity;
+    void Awake() 
     {
-        if(other.tag != "Fence")
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update() 
+    {
+        currentVelocity = rb.velocity;
+    }
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.gameObject.tag != "Fence" && other.gameObject.tag != "Wall")
             Destroy(gameObject);
+        else
+        {
+            Vector2 inDirection = currentVelocity;
+            Vector2 inNormal = other.contacts[0].normal;
+            Vector2 newVelocity;
+            // if(other.contacts[0].normal.x < -Mathf.Epsilon)
+            // {
+            //     Debug.Log("Hit Side");
+            //     newVelocity = new Vector2(-inDirection.x, inDirection.y);
+            // }
+            // else
+            // {
+            //     Debug.Log("Hit Top/Bottom");
+            //     newVelocity = new Vector2(inDirection.x, -inDirection.y);
+            // }
+            newVelocity = Vector2.Reflect(inDirection, inNormal);
+            rb.velocity = newVelocity;
+        }
     }
 }
